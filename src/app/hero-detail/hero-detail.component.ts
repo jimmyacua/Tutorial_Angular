76220edcc0a8f82a80ca20000d795317292b0414
jimmyacua <jimmyacua@hotmail.com>
2019-05-01 20:46:36 -0600

    finalizado hasta http

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -30,6 +30,19 @@ goBack(): void {
   this.location.back();
 }
 
+save(): void {
+   this.heroService.updateHero(this.hero)
+     .subscribe(() => this.goBack());
+ }
+
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
 	@Input() hero: Hero;
 
 }
